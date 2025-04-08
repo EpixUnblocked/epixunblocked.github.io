3ecb8ec372b3eec4db5d40f406372c29ad17bb18
Coke <Zimo.gu4@gmail.com>
2025-04-08 18:39:05 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
+// pages/_app.js
+import '../styles/globals.css';
+import Layout from '../components/Layout';
+import { GameProvider } from '../context/GameContext';
 
-import '../styles/globals.css'
-import Layout from '../components/Layout'
-
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <GameProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </GameProvider>
+  );
 }
-
-export default MyApp
