e1279ac93377485a520eaf785be3c6544cad812b
Coke <Zimo.gu4@gmail.com>
2025-04-08 16:08:43 +1000

    Update [slug].js

--- pages/games/[slug].js
+++ pages/games/[slug].js
@@ -2,6 +2,22 @@
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 
+import Link from 'next/link';
+
+...
+
+return (
+  <>
+    <Link href="/" style={{ color: '#fff', marginBottom: '12px', display: 'inline-block' }}>← Back to Game List</Link>
+
+    <h1>{game.title}</h1>
+    <iframe
+      src={`/games/${game.slug}/index.html`}
+      style={{ width: '100%', height: '80vh', border: 'none', borderRadius: '10px' }}
+    />
+  </>
+);
+
 export default function Game() {
   const router = useRouter()
   const { slug } = router.query
