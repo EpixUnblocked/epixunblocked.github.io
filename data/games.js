9968d05a3efb591b22a749dff799162d1d8e3e73
Coke <Zimo.gu4@gmail.com>
2025-04-08 16:24:08 +1000

    Create games.js

---
+++ data/games.js
@@ -0,0 +1,14 @@
+// /data/games.js
+
+const games = [
+  {
+    title: 'Flappy Bird',
+    slug: 'flappy',
+    description: 'A fun and addictive clone of the classic Flappy Bird game.',
+    thumbnail: '/games/flappy/thumbnail.png',
+  },
+  
+  // Add more games here...
+];
+
+export default games;
