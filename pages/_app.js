0b27b6add6886e347f23cd1f8e60a5b002efbb38
Coke <Zimo.gu4@gmail.com>
2025-04-10 16:16:19 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,15 +8,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-H1LHBQVY1T"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-H1LHBQVY1T');
-</script>
         <title>Epix</title>
         <link rel="icon" href="/epix/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
