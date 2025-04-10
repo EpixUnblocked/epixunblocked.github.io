63b87e703cb2ec72f816f23db7f2e06c87f0905f
Coke <Zimo.gu4@gmail.com>
2025-04-10 13:11:20 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,11 @@ import { GameProvider } from '../context/GameContext';
 
 export default function App({ Component, pageProps }) {
   return (
+    <Head>
+        <title>Epix</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
     <GameProvider>
       <Layout>
         <Component {...pageProps} />
