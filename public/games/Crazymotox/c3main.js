5108f570c0bf7f928fd11604b672bafa0dfdc148
Void <voidpixelated@gmail.com>
2025-04-13 20:49:48 +1000

    Add files via upload

---
+++ public/games/Crazymotox/c3main.js
@@ -0,0 +1,6 @@
+import "./../colyseus.js";
+import "./../box2d.wasm.js";
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/main.js";
+import "./project/scriptsInEvents.js";
---
+++ public/games/Crazymotox/main2.js
@@ -0,0 +1,12 @@
+
+// Import any other script files here, e.g.:
+// import * as myModule from "./mymodule.js";
+
+runOnStartup(async runtime =>
+{
+	// Code to run on the loading screen.
+	// Note layouts, objects etc. are not yet available.
+	
+});
+
+window.addEventListener("keydown", function(e) { if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) { e.preventDefault(); } }, false);
