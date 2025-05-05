e0d5fa7c00674e908d8ed4b558f584db475c5a9d
Coke <Zimo.gu4@gmail.com>
2025-05-05 17:47:50 +1000

    Create TabCloak.js

---
+++ components/TabCloak.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+
+export default function TabCloak() {
+  useEffect(() => {
+    const originalTitle = document.title;
+    const originalFavicon = document.querySelector("link[rel='icon']")?.href;
+
+    const cloakTitle = 'Google Docs';
+    const cloakFavicon = 'https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico';
+
+    const changeFavicon = (src) => {
+      let link = document.querySelector("link[rel='icon']");
+      if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+      }
+      link.href = src;
+    };
+
+    const handleBlur = () => {
+      document.title = cloakTitle;
+      changeFavicon(cloakFavicon);
+    };
+
+    const handleFocus = () => {
+      document.title = originalTitle;
+      if (originalFavicon) changeFavicon(originalFavicon);
+    };
+
+    window.addEventListener('blur', handleBlur);
+    window.addEventListener('focus', handleFocus);
+
+    return () => {
+      window.removeEventListener('blur', handleBlur);
+      window.removeEventListener('focus', handleFocus);
+    };
+  }, []);
+
+  return null;
+}
