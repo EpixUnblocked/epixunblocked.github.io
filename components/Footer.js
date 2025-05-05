524d9e2ea461b51f73582c974feb93c9afa9e3b9
Coke <Zimo.gu4@gmail.com>
2025-05-06 00:10:13 +1000

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,21 @@
+// components/Footer.js
+
+import styles from '../styles/Footer.module.css';
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.inner}>
+        <div className={styles.links}>
+          <Link href="/terms"><span>Terms of Service</span></Link>
+          <Link href="/privacy"><span>Privacy Policy</span></Link>
+          <a href="https://github.com/EpixUnblocked" target="_blank" rel="noopener noreferrer">
+            <span>GitHub</span>
+          </a>
+        </div>
+        <p className={styles.copyright}>© {new Date().getFullYear()} Epix. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
