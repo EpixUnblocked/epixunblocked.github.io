14ab28609bbe596f42ad291029962166fe58ae8e
Coke <Zimo.gu4@gmail.com>
2025-05-06 22:12:22 +1000

    add meta

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,10 @@ export default function App({ Component, pageProps }) {
           <title>Epix</title>
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:title" content="Epix Unblocked" />
+          <meta property="og:description" content="Epix Unblocked games has a wide range of games that bypasses your school or workplace internet restrictions and hides itself once you exit the tab." />
+          <meta property="og:url" content="https://epixunblocked.github.io/" />
+          <meta property="og:type" content="website" />
         </Head>
         <Layout>
           <Component {...pageProps} />
