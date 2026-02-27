11534a3264e672010708cb2df840ea0ccb96ddee
Coke <Zimo.gu4@gmail.com>
2026-02-27 11:28:29 +1100

    Update favicon source in TabCloak.js (#6)

--- components/TabCloak.js
+++ components/TabCloak.js
@@ -9,8 +9,7 @@ export default function TabCloak() {
     const originalFavicon = document.querySelector("link[rel='icon']")?.href;
 
     const cloakTitle = 'Google Drive';
-    // const cloakFavicon = 'https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico';
-    const cloakFavicon = '/googledrive.ico';
+    const cloakFavicon = 'https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico';
 
     const changeFavicon = (src) => {
       let link = document.querySelector("link[rel='icon']");
